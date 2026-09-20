ec76a760119837b053f7e818b686f9117c9c89ab
mrazurecoach <azurecoach2030@gmail.com>
2026-09-20 16:26:09 +0530

    Update Terraform configuration

--- main.tf
+++ main.tf
@@ -12,15 +12,15 @@ resource "azurerm_resource_group" "trf-rgp" {
 
 # Create a virtual network
   resource "azurerm_virtual_network" "trf-vnet" {
-  resource_group_name = azurerm_resource_group.trf-rgpn.name  
+  resource_group_name = azurerm_resource_group.trf-rgp.name  
   name                = var.vnet
-  location            = azurerm_resource_group.trf-rgpn.location
+  location            = azurerm_resource_group.trf-rgp.location
   address_space       = ["10.20.0.0/16"]
 }
 
 
 resource "azurerm_subnet" "trf-subnet" {
-  resource_group_name  = azurerm_resource_group.trf-rgpn.name
+  resource_group_name  = azurerm_resource_group.trf-rgp.name
   virtual_network_name = azurerm_virtual_network.trf-vnet.name
   name                 =  "web-subnet"
   address_prefixes     = ["10.1.0.0/24"]
